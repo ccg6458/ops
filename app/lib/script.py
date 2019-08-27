d95ec9b540d284b30c9d2496ebe62e3b77e296a0
service <service@mifengkong.cn>
2019-08-27 19:30:17 +0800

    调整数据库配置

--- app/lib/script.py
+++ app/lib/script.py
@@ -2,25 +2,14 @@ import pymysql
 from app.config import Config
 import paramiko
 
-private_key = paramiko.RSAKey.from_private_key_file(Config.PRIVATE_KEY)
-
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-ssh.connect(hostname='119.23.161.45', port=22, username="root", pkey=private_key)
-
-stdin, stdout, stderr = ssh.exec_command('ls -l /')
-
-result = stdout.read().decode()
-
-ssh.close()
-
 
 class Mymysql():
     success = 1
 
     def __init__(self):
-        self.db = pymysql.connect("localhost", "root", "mifeng888", "test")
+        mysql = Config.MYSQL_INFO
+        self.db = pymysql.connect(mysql['url'], mysql['user'],
+                                  mysql['password'], mysql['database'])
         self.cursor = self.db.cursor()
 
     def execute_sql(self, sql):
