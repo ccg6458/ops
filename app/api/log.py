602e4dac6b3ee285ccfdc1bc8c34f049de31200b
service <service@mifengkong.cn>
2019-09-02 16:38:36 +0800

    日志分页

--- app/api/log.py
+++ app/api/log.py
@@ -1,19 +1,25 @@
 from . import SecurityResource
-from flask_login import current_user
 from app.model.log import LogModel
+from sqlalchemy import text
+from flask import request
 
 
 class Log(SecurityResource):
-
     super_api = True
 
     def get(self):
         super(Log, self).get()
+        args = request.args
+        pageSize = int(args.get('pageSize'))
+        pageNum = int(args.get('pageNum'))
+        offsetNum = (pageNum - 1) * pageSize if pageSize > 1 else 0
+        total = len(LogModel.query.all())
+        data = {}
         log_list = []
-        if current_user.is_super():
-            logs = LogModel.query.all()
-            for log in logs:
-                log_list.append(log.to_json())
-            return self.render_json(data=log_list)
+        logs = LogModel.query.order_by(LogModel.create_time.desc()).limit(pageSize).offset(offsetNum).all()
+        for log in logs:
+            log_list.append(log.to_json())
+        data['total'] = total
+        data['log'] = log_list
 
-
+        return self.render_json(data=data)
