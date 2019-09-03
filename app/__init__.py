ddbdcdd1ebcb8db76eb4433dd7d9828735bf8363
service <service@mifengkong.cn>
2019-09-03 14:05:05 +0800

    session模式调整为默认

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
-from flask import Flask,g
+from flask import Flask, g
 from flask_restful import Api
 from app.config import Config
 from app.api.user import UserApi
 from app.api.perm import PermApi
-from app.api.login import Login,Logout
+from app.api.login import Login, Logout
 from app.api.sidebar import SideBarApi
 from app.api.business import BusinessApi
 from app.api.task import TaskApi
@@ -16,7 +16,7 @@ from app.model.user import UserModel
 from app.model.task import TaskModel
 from app.lib.extensions import CronJob
 
-from app.lib.extensions import db, login_manager,cron
+from app.lib.extensions import db, login_manager, cron
 from flask_cors import CORS, cross_origin
 from flask_login import login_required
 
@@ -29,7 +29,6 @@ def create_app():
     CORS(app, supports_credentials=True)
     register_cron(app)
 
-
     return app
 
 
@@ -41,38 +40,33 @@ def register_extensions(app):
         return UserModel.query.get(user_id)
 
     login_manager.init_app(app)
-    login_manager.session_protection = 'strong'
 
     return app
 
 
 def register_url(app):
     api = Api(app)
-    api.add_resource(UserApi, '/test/', '/test/<string:tag>','/test/<int:id>' ,endpoint='user')
+    api.add_resource(UserApi, '/test/', '/test/<string:tag>', '/test/<int:id>', endpoint='user')
     api.add_resource(PermApi, '/perm/<int:id>', endpoint='perm')
     api.add_resource(Login, '/login', endpoint='login')
     api.add_resource(Logout, '/logout', endpoint='logout')
     api.add_resource(SideBarApi, '/sidebar', endpoint='sidebar')
-    api.add_resource(BusinessApi, '/business','/business/<string:tag>','/business/<string:tag>/<int:id>','/business/modify/<int:id>', endpoint='business')
-    api.add_resource(TaskApi, '/task','/task/<int:id>', endpoint='task')
+    api.add_resource(BusinessApi, '/business', '/business/<string:tag>', '/business/<string:tag>/<int:id>',
+                     '/business/modify/<int:id>', endpoint='business')
+    api.add_resource(TaskApi, '/task', '/task/<int:id>', endpoint='task')
     api.add_resource(Log, '/log', endpoint='audit')
     api.add_resource(SendMsgApi, '/sendmsg', endpoint='sendmsg')
     api.add_resource(EmailLogin, '/emaillogin', endpoint='emaillogin')
-    api.add_resource(WorkOrderApi, '/work', '/work/<string:tag>',endpoint='work')
-
-
-
-
+    api.add_resource(WorkOrderApi, '/work', '/work/<string:tag>', endpoint='work')
 
     return app
 
+
 def register_cron(app):
-    task_list=[]
+    task_list = []
     with app.app_context():
         tasks = TaskModel.query.all()
         for task in tasks:
             task_list.append(task.to_json())
     cron.start(task_list)
     return app
-
-
